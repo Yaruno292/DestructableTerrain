efbabd3b257f1738dcbb6cbadfeb5838f4c166e3
Yaruno292 <Yarunoyo@outlook.com>
2018-09-26 14:00:50 +0200

    The scripts i coded
    Player scripts, weapon scripts, ui scripts, terrain scripts,
    destructables

---
+++ Scripts/UI/Scope.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour {
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private GameObject scopeOverlay;
+
+    [SerializeField]
+    private Camera weaponCam;
+
+    [SerializeField]
+    private Camera mainCam;
+
+    private float normalFov;
+    private float scopedFov = 15f;
+
+    private bool isScoped = false;
+
+    private void Start()
+    {
+        weaponCam.GetComponent<Camera>();
+        normalFov = mainCam.fieldOfView;
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            isScoped = !isScoped;
+            animator.SetBool("Scoped", isScoped);
+            if(isScoped)
+            {
+                StartCoroutine(TriggerScope());
+            }
+            else
+            {
+                UnScope();
+            }
+        }
+	}
+
+    void UnScope()
+    {
+        scopeOverlay.SetActive(false);
+        weaponCam.enabled = true;
+        mainCam.fieldOfView = normalFov;
+    }
+
+    IEnumerator TriggerScope()
+    {
+        yield return new WaitForSeconds(0.15f);
+        scopeOverlay.SetActive(true);
+        weaponCam.enabled = false;
+        mainCam.fieldOfView = scopedFov;
+    }
+}
---
+++ Scripts/Weapon/WeaponSwitch.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+
+    private PlayerShoot shoot;
+
+    private void Start()
+    {
+        shoot = GetComponent<PlayerShoot>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //Handgun
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            shoot.SwitchWeapon(0,15,false,1,0.3f,30, 100f, 230f);
+        }
+
+        //M4
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            shoot.SwitchWeapon(1,45,true,2,0.1f,10, 150f, 240f);
+        }
+
+        //Shotty
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shoot.SwitchWeapon(2,5,false,2,0.8f,60, 50f, 280f);
+        }
+
+        //AK
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            shoot.SwitchWeapon(3,30,true,2,0.2f,20, 120f, 240f);
+        }
+
+        //Sniper
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            shoot.SwitchWeapon(4,6,false,3,1f,90, 300f, 300f);
+        }
+
+        /*
+        //RPG
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            shoot.SwitchWeapon(5,1,false,6,0f,90, 100f, 320f);
+        }
+
+        //DubstepGun
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            shoot.SwitchWeapon(6,100,true,3,0.8f,40, 75f, 240f);
+        }
+        */
+    }
+}
